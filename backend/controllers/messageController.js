36f90ba24b6bee677e534235c5969812d70eb8a5
Kutlu <kutlu.karakoc@akinon.com>
2025-03-28 01:25:42 +0300

    refactor: update imports in messageController and qrController to use destructuring from whatsapp-web.js package

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,4 +1,6 @@
-import { Client, LocalAuth } from "whatsapp-web.js";
+import pkg from 'whatsapp-web.js';
+
+const { Client, LocalAuth } = pkg;
 
 const client = new Client({ authStrategy: new LocalAuth() });
 client.initialize();
