9cf8f9b1e5cca043d1aad68fa671d57ac651f6d0
Kutlu <kutlu.karakoc@akinon.com>
2025-03-28 01:16:21 +0300

    feat: add Toaster component for notifications and integrate it into the main App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import QRPage from "./pages/QR";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/qr" element={<QRPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/qr" element={<QRPage />} />
+        </Routes>
+      </Router>
+      <Toaster />
+    </>
   );
 }
 
